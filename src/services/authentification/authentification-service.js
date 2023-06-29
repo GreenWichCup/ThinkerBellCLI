582e302eb6d2fa5507a6c1e45ab1308a4b2bae3d
GreenWichCup <53257193+GreenWichCup@users.noreply.github.com>
2023-06-29 15:22:52 -0400

    Update authentification-service.js
    Removed API infos

--- src/services/authentification/authentification-service.js
+++ src/services/authentification/authentification-service.js
@@ -3,12 +3,12 @@ import firebase from '@react-native-firebase/app';
 
 const firebaseConfig = {
   apiKey: "My-API-Key",
-  authDomain: "tatthood-ee0f3.firebaseapp.com",
-  databaseURL: "https://tatthood-ee0f3-default-rtdb.firebaseio.com",
-  projectId: "tatthood-ee0f3",
-  storageBucket: "tatthood-ee0f3.appspot.com",
-  messagingSenderId: "439606340009",
-  appId: "1:439606340009:web:b8c4b3426130ea7473bcb8",
-  measurementId: "G-ERKT9PG4FH",
+  authDomain: "My-authDomain",
+  databaseURL: "My-databaseURL",
+  projectId: "My-projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
+  measurementId: "measurementId",
 };
 
